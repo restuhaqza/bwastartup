727dc8672f0b9a0eacf1eee74780428c7f946f9a
Restu Haqqi Muzakir <restuhaqza@gmail.com>
2021-02-14 21:36:42 +0700

    feat: add register user endpoint

---
+++ handler/user.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		var errors []string
+		errors = helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Account register is failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.APIResponse("Account register is failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("User created", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	/**
+	* Connecting to database
+	 */
+	dsn := "root:passw0rd@tcp(127.0.0.1:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// repository instance
+	userRepository := user.NewRepository(db)
+
+	// service instance
+	userService := user.NewService(userRepository)
+
+	// handler instance
+	userHandler := handler.NewUserHandler(userService)
+
+	// init gin router
+	router := gin.Default()
+
+	// set api group for `/api/v1`
+	api := router.Group("/api/v1")
+
+	// list of router
+	api.POST("/users", userHandler.RegisterUser)
+
+	// listen server on port 3001
+	router.Run(":3001")
+
+}
